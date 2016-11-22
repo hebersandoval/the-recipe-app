9bbbd7837985668549756318896b36dd26d94826
Heber Sandoval <he3ers@gmail.com>
2016-11-22 16:45:51 -0500

    Show action add to display created user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
 
   def create
-    byebug
     @user = User.new(user_params)
     if @user.save
       redirect_to user_path(@user)
