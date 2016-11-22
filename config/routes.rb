e3a833b7b3ff07e003a9f70882bac223c305228e
Heber Sandoval <he3ers@gmail.com>
2016-11-22 17:03:59 -0500

    Add nested resources.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'home_page#home'
-  resources :users
+  
+  resources :users do
+    resources :recipes
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
