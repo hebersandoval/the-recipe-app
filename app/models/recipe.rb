1c1a903717b24bc8d5cdeda05ba948115f0eee3c
Heber Sandoval <he3ers@gmail.com>
2016-11-22 15:39:41 -0500

    Add has_many through: on the recipe model.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,7 @@
 class Recipe < ActiveRecord::Base
   has_many :reviews
-  has_many :users, :through :reviews, dependent: :destroy
+  has_many :users, through: :reviews, dependent: :destroy
+
+  has_many :pantries
+  has_many :ingredients, through: :pantries
 end
