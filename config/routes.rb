b918c9fdc82bc8138470dd0e2c314ecee9c27ff1
Heber Sandoval <he3ers@gmail.com>
2016-11-24 12:25:32 -0500

    Edit page for reviews added.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users
 
   resources :recipes, only: [:index, :show] do
-    resources :reviews, only: [:index, :show, :new]
+    resources :reviews, only: [:index, :show, :new, :edit]
   end
 
   resources :reviews
