4d810b23ee5d5cab4eb8c0a27d0f0c3c466030e9
Heber Sandoval <he3ers@gmail.com>
2016-12-01 12:13:06 -0500

    'correct_user before filter added.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
 
   def index
     @users = User.all
@@ -48,5 +49,16 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # Before filters
+
+  # Confirms the correct user
+  def correct_user
+    @user = User.find(params[:id])
+    if !current_user?(@user)
+      flash[:danger] = "You don't have permission to access this page!"
+    end
+    redirect_to(root_path) unless current_user?(@user)
   end
 end
