1551aa16ce49f02bcf57e0c45e09cad86c6a323d
Heber Sandoval <he3ers@gmail.com>
2016-12-01 10:56:15 -0500

    Can create a new user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
+      flash[:succes] = "Welcome to the Recipe App!"
       redirect_to user_path(user)
     else
       render :new
@@ -23,6 +24,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home_page#home'
+  get 'signup', to: 'users#new'
 
   resources :users
 
