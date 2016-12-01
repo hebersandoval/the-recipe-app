56d083a79f398f61cb4a3816efcaef22cf3f88b0
Heber Sandoval <he3ers@gmail.com>
2016-12-01 11:34:27 -0500

    Create session helpers and log_out method for user.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in user
       flash[:succes] = "Hey, good lookin', whatcha got cookin'?"
       redirect_to user_path(user)
     else
       flash.now[:danger] = "Invalid email/password combination!"
       render :new
     end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_path
   end
 end
