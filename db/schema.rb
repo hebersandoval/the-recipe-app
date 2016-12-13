5479a782659dcdaaa25dae316d533e9d5f9dec9d
Heber Sandoval <he3ers@gmail.com>
2016-12-13 16:16:10 -0500

    Add user_id column to recipes table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161201152716) do
+ActiveRecord::Schema.define(version: 20161213204810) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20161201152716) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
