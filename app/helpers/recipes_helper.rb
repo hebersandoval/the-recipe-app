6efc91763edb5d9e778a60bb0d4ff827d3bc6c33
Heber Sandoval <he3ers@gmail.com>
2016-12-19 10:00:09 -0500

    Select box for user's in recipe's new page.

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,9 @@
 module RecipesHelper
+  def user_id_field(recipe)
+    if recipe.user.nil?
+      select_tag "recipe[user_id]", options_from_collection_for_select(User.all, :id, :name)
+    else
+      hidden_field_tag "recipe[user_id]", recipe.user_id
+    end
+  end
 end
